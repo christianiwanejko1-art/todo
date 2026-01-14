d51c992bdd0f392a6d66ed7174ff000a4f8ff161
Christian Iwanejko <christianiwanejko1@gmail.com>
2026-01-13 18:28:15 -0800

    updated styling of website

--- src/home.js
+++ src/home.js
@@ -6,15 +6,15 @@ const createHome = function () {
     const main = document.createElement('div');
     main.id = 'main';
 
-    
-    const nav = document.createElement('nav');
+    const navBar = document.createElement('div')
+    navBar.classList.add('navBar');
     const ul = document.createElement('ul');
     ul.id = 'navUl';
     let arr = ['Dashboard','Tasks','Notes','Email','Calender','Reports','Contacts','Invite Team'];
     const logo = document.createElement('div');
     logo.id = 'logo';
     const logoH1 = document.createElement('h1');
-    logoH1.textContent = 'Logo';
+    logoH1.textContent = 'To do List';
     logo.appendChild(logoH1);
     const navSide = document.createElement('div');
     navSide.id = 'navSide';
@@ -25,34 +25,48 @@ const createHome = function () {
         li.textContent = arr[i]
         ul.appendChild(li);
     }
-    const team = document.createElement('div');
-    team.id = 'workspace';
-    const teamLogo = document.createElement('div');
-    const teamTitle = document.createElement('h1');
-    teamTitle.textContent = 'Perception';
-    const teamWorkspace = document.createElement('p');
-    teamWorkspace.textContent = 'Workspace';
-    team.append(teamLogo,teamTitle,teamWorkspace);
+
     navSide.append(h1Mainmenu, ul)
-    nav.append(logo,navSide,team);
-    main.appendChild(nav);
+    navBar.append(logo, navSide)
+    main.appendChild(navBar);
 
     const tasksContainer = document.createElement('div');
     tasksContainer.id = 'tasksContainer';
+    const addDiv = document.createElement('div');
+    addDiv.classList.add('addDiv');
+    const addBtn = document.createElement('button');
+    addBtn.classList.add('addBtn');
+    addBtn.textContent = 'Add Task'
+    addDiv.appendChild(addBtn);
+    const title = document.createElement('h1');
+    title.classList.add('title');
+    title.textContent = 'Tasks List';
     const tasksInprogress = document.createElement('div');
     tasksInprogress.classList.add('tasksInprogress');
+    const tasksinprogressH1 = document.createElement('h1');
+    tasksinprogressH1.classList.add('tasksinprogressH1');
+    tasksinprogressH1.textContent = 'In Progress';
+    tasksInprogress.appendChild(tasksinprogressH1);
     const tasksInProgressWrapper = document.createElement('div')
     tasksInProgressWrapper.classList.add('tasksInProgressWrapper')
     tasksInProgressWrapper.append(tasksInprogress)
 
     const tasksInreview = document.createElement('div');
     tasksInreview.classList.add('tasksInreview');
+    const tasksinreviewH1 = document.createElement('h1');
+    tasksinreviewH1.classList.add('tasksinreviewH1');
+    tasksinreviewH1.textContent = 'In Review';
+    tasksInreview.appendChild(tasksinreviewH1);
     const tasksInReviewWrapper = document.createElement('div')
     tasksInReviewWrapper.classList.add('tasksInReviewWrapper')
     tasksInReviewWrapper.append(tasksInreview)
 
     const tasksCompleted = document.createElement('div');
     tasksCompleted.classList.add('tasksCompleted');
+    const taskscompletedH1 = document.createElement('h1');
+    taskscompletedH1.classList.add('taskscompletedH1');
+    taskscompletedH1.textContent = 'Completed';
+    tasksCompleted.appendChild(taskscompletedH1);
     const tasksCompletedWrapper = document.createElement('div')
     tasksCompletedWrapper.classList.add('tasksCompletedWrapper')
     tasksCompletedWrapper.append(tasksCompleted)
@@ -65,7 +79,7 @@ const createHome = function () {
     tasksInner.id = 'tasksInner';
 
     tasksInner.append(tasksInProgressWrapper, tasksInReviewWrapper, tasksCompletedWrapper)
-    tasksContainer.appendChild(tasksInner)
+    tasksContainer.append(tasksInner,title,addDiv)
 
     main.appendChild(tasksContainer)
 
@@ -78,6 +92,7 @@ const createHome = function () {
 function addCardProgress(title, summary) {
     const progress = document.querySelector('.tasksInprogress'); // or .tasksInprogress
     const cardDiv = document.createElement('div');
+    cardDiv.classList.add('card');
 
     const cardH1 = document.createElement('h1');
     cardH1.textContent = title;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import "./styles.css";
 import { createHome, addCardProgress } from "./home.js";
 
+console.log('js loaded')
+
 document.addEventListener("DOMContentLoaded", () => {
     createHome()
-    addCardProgress('random','card')
+    addCardProgress('random','card');
+    const addBtn = document.querySelector('.addBtn');
+    addBtn.addEventListener('click', () => {
+        console.log('btn clicked');
+    })
 });
+
